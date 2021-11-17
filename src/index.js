33ce0e5e9ae5f35d5de90e0ac095ddcd7fb2a8eb
Nicolas Trujillo <tnicolas@uninorte.edu.co>
2021-11-17 17:31:42 -0500

    Primer Commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { saludar } from './js/componentes';
+import './style.css';
+
+const nombre = 'Nicolás';
+saludar(nombre);
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin    = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin           = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    output: {
+        clean: true
+    },
+
+    module: {
+        rules: [
+
+            {
+
+                test:/\.html$/,
+                loader: 'html-loader', 
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /style.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /style.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+        
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                  },
+                ],
+            },
+
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template:'./src/index.html',
+            filename:'./index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename:'[name].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "src/assets/", to: "assets/" },
+            ],
+        }),
+    ]
+}
